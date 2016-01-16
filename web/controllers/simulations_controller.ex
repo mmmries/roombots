5f9d7e6948bd434166c93c87c51b355e0271d358
Michael Ries <michael@riesd.com>
2016-01-16 14:40:39 -0700

    link to simulator and some better instructions

--- web/controllers/simulations_controller.ex
+++ web/controllers/simulations_controller.ex
@@ -2,7 +2,6 @@ defmodule Recruitbots.SimulationsController do
   use Recruitbots.Web, :controller
 
   def index(conn, %{}) do
-    conn = put_layout(conn, "simulation.html")
     render conn, "index.html"
   end
 end
