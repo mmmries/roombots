40a70d1891ded211642da57e72404e4e4b04be9b
Michael Ries <michael@riesd.com>
2016-01-15 23:59:51 -0700

    drawing the roomba

--- web/static/js/simulation.js
+++ web/static/js/simulation.js
@@ -1,10 +1,17 @@
 const WIDTH = 750;
 const HEIGHT = 750;
+const ROOMBA_RADIUS = WIDTH * 150 / 10000
 
 let canvas = document.getElementById("simulation")
 if(canvas) {
   let context = canvas.getContext("2d")
   let board = generateBoard()
+  drawBoard(context, board)
+  drawRoomba(context, 5000, 5000, 0)
+}
+
+function drawBoard(context, board) {
+  context.save()
   context.fillStyle = "#000"
   for(var row in board){
     for(var col in board[row]) {
@@ -12,6 +19,7 @@ if(canvas) {
       drawCell(context, row, col, cell)
     }
   }
+  context.restore()
 }
 
 function drawCell(context, row, col, cell){
@@ -20,6 +28,26 @@ function drawCell(context, row, col, cell){
     let y = col * (WIDTH / 100)
     context.fillRect(x, y, WIDTH / 100, HEIGHT / 100)
   }
+}
+
+function drawRoomba(context, x_mm, y_mm, heading){
+  context.save()
+  let x = x_mm * (WIDTH / 10000)
+  let y = y_mm * (HEIGHT / 10000)
+  context.translate(x, y)
+  context.rotate(heading)
+  context.fillStyle = "#000"
+  context.beginPath()
+  context.arc(0, 0, ROOMBA_RADIUS, 0, Math.PI * 2, true)
+  context.stroke()
+  context.beginPath()
+  context.arc(0, 0, ROOMBA_RADIUS*0.7, -Math.PI / 3, Math.PI / 3, false)
+  context.stroke()
+  context.beginPath()
+  context.lineWidth = 3
+  context.arc(0, 0, ROOMBA_RADIUS, -Math.PI / 3, Math.PI / 3, false)
+  context.stroke()
+  context.restore()
 }
 
 function generateBoard(){
