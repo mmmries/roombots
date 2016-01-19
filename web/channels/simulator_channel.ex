947a29557badbef615c3a038d58c0764e5d61b83
Michael Ries <michael@riesd.com>
2016-01-19 14:34:19 -0700

    forward sensor_updates

--- web/channels/simulator_channel.ex
+++ web/channels/simulator_channel.ex
@@ -2,4 +2,10 @@ defmodule Recruitbots.SimulatorChannel do
   use Phoenix.Channel
 
   def join("simulator:"<>id, _auth, socket), do: {:ok, assign(socket, :id, id)}
+
+  def handle_in("sensor_update", sensors, socket) do
+    id = socket.assigns.id
+    Recruitbots.Endpoint.broadcast("simulation:"<>id, "sensor_update", sensors)
+    {:reply, {:ok, %{}}, socket}
+  end
 end
