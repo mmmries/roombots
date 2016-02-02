832f8147133d15bbba77a59b37ff069416c83e3c
Michael Ries <michael@riesd.com>
2016-02-02 09:54:41 -0500

    do not check origin for websocket connections

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule Recruitbots.UserSocket do
   channel "simulation:*", Recruitbots.SimulationChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
