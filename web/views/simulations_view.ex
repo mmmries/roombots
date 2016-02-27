c4023463f3a978e76593ef912a432c927da52615
hqmq <michael@riesd.com>
2016-02-27 14:38:42 -0700

    use the configured hostname in the simulation instructions
    Fixes #12

--- web/views/simulations_view.ex
+++ web/views/simulations_view.ex
@@ -1,3 +1,10 @@
 defmodule Recruitbots.SimulationsView do
   use Recruitbots.Web, :view
+
+  def socket_url do
+    base_url = Recruitbots.Router.Helpers.page_url(Recruitbots.Endpoint, :index)
+    |> String.replace("http", "ws")
+
+    "#{base_url}socket/websocket"
+  end
 end
